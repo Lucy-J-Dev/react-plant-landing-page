8bd1e4da0c328c36c7714f71ee1ea8d52f306928
Lucy Jiménez <lucy.jimenez.mejia@gmail.com>
2024-08-24 12:53:41 -0500

    instalación de tipografias, configuración y verificación de uso en aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 const App = () => {
   return (
-    <h1 className="text-4xl font-bold bg-green-500 sm:bg-red-400 md:bg-blue-400 lg:bg-yellow-400 xl:bg-purple-400 text-white container">
-      Hello Tailwind CSS !!!
-    </h1>
+    <div>
+      <h1 className="text-4xl font-bold bg-green-500 sm:bg-red-400 md:bg-blue-400 lg:bg-yellow-400 xl:bg-purple-400 text-white container font-Jost">
+        Hello Tailwind CSS !!!
+      </h1>
+
+      <h1 className="text-4xl font-bold bg-green-500 sm:bg-red-400 md:bg-blue-400 lg:bg-yellow-400 xl:bg-purple-400 text-white container font-Lobster">
+        Hello Tailwind CSS !!!
+      </h1>
+    </div>
   );
 };
 
