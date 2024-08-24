684ef19525ea9f64525f2155310f285e22e0d0be
Lucy Jiménez <lucy.jimenez.mejia@gmail.com>
2024-08-24 12:47:04 -0500

    prueba configuración pantallas por defecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <h1 className="text-4xl font-bold text-rose-600">Hello Tailwind CSS !!!</h1>;
+  return (
+    <h1 className="text-4xl font-bold bg-green-500 sm:bg-red-400 md:bg-blue-400 lg:bg-yellow-400 xl:bg-purple-400 text-white container">
+      Hello Tailwind CSS !!!
+    </h1>
+  );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: "340px",
+      md: "540px",
+      lg: "768px",
+      xl: "1180px"
+    },
     extend: {},
+    container: {
+      center: true,
+      padding: {
+        default: "12px",
+        md: "32px"
+      }
+    }
   },
   plugins: [],
 }
